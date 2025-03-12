eb2a4b52bb2b16bd2fc015db4eb496e1f9bf5619
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-03-12 20:24:26 +0530

    dynamic url

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return "Hello World"
 
+@app.route('/greet/<name>')
+def nam(name):
+  return f"Hello {name}"
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port='3000', debug=True)
