f56e2228a549cd4727b095532c1a3c85cceb767c
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-03-12 20:47:52 +0530

    rendering html file

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-  return "Hello World"
+  myvalue = 'Yo bro'
+  myresult = 10 + 20
+  return render_template('index.html', myresult=myresult, myvalue=myvalue)
 
 @app.route('/greet/<name>')
 def nam(name):
