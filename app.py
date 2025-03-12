bcb94a6853002fd96268b0db922ae3c026992587
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-03-13 00:09:54 +0530

    handling json data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Response, send_from_directory
+from flask import Flask, render_template, request, Response, send_from_directory, jsonify
 import pandas as pd
 import os
 import uuid
@@ -57,6 +57,16 @@ def convert_csv_two():
 @app.route('/download/<filename>')
 def download(filename):
   return send_from_directory('downloads', filename, download_name='result.csv')
+
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+  greeting = request.json['greeting']
+  name = request.json['name']
+  
+  with open('file.txt', 'w') as f:
+    f.write(f'{greeting}, {name}')
+    
+  return jsonify({'message': 'Successfully written!'})
 
 @app.route('/greet/<name>')
 def nam(name):
