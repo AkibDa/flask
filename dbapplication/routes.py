378d79856a31bf5a654f612e6a48d8a69a116e03
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-03-13 23:08:06 +0530

    disabled Person model

--- dbapplication/routes.py
+++ dbapplication/routes.py
@@ -3,40 +3,40 @@ from models import Person
 from app import db
 
 def register_routes(app, db):
-    @app.route('/', methods=['GET', 'POST'])
-    def index():
-        if request.method == 'POST':
-            name = request.form.get('name')
-            age = request.form.get('age')
-            job = request.form.get('job')
+    # @app.route('/', methods=['GET', 'POST'])
+    # def index():
+    #     if request.method == 'POST':
+    #         name = request.form.get('name')
+    #         age = request.form.get('age')
+    #         job = request.form.get('job')
 
-            if not name or not age or not job:
-                return "All fields are required!", 400
+    #         if not name or not age or not job:
+    #             return "All fields are required!", 400
 
-            try:
-                age = int(age)
-            except ValueError:
-                return "Age must be a number!", 400  
+    #         try:
+    #             age = int(age)
+    #         except ValueError:
+    #             return "Age must be a number!", 400  
 
-            person = Person(name=name, age=age, job=job)
-            db.session.add(person)
-            db.session.commit()
+    #         person = Person(name=name, age=age, job=job)
+    #         db.session.add(person)
+    #         db.session.commit()
 
-            return redirect(url_for('index'))  # Redirect to avoid resubmission
+    #         return redirect(url_for('index'))  # Redirect to avoid resubmission
 
-        people = Person.query.all()
-        return render_template('index.html', people=people)
+    #     people = Person.query.all()
+    #     return render_template('index.html', people=people)
 
-    @app.route('/delete/<pid>', methods=['DELETE'])
-    def delete(pid):
-        Person.query.filter(Person.pid == pid).delete()
+    # @app.route('/delete/<pid>', methods=['DELETE'])
+    # def delete(pid):
+    #     Person.query.filter(Person.pid == pid).delete()
         
-        db.session.commit()
+    #     db.session.commit()
         
-        people = Person.query.all()
-        return render_template('index.html', people=people)
+    #     people = Person.query.all()
+    #     return render_template('index.html', people=people)
     
-    @app.route('/details/<pid>')
-    def details(pid):
-        person = Person.query.filter(Person.pid == pid).first()
-        return render_template('details.html', person=person)
+    # @app.route('/details/<pid>')
+    # def details(pid):
+    #     person = Person.query.filter(Person.pid == pid).first()
+    #     return render_template('details.html', person=person)
