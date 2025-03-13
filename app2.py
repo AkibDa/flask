e34ce2ad6075105a7d31ab49fb09cda7925ad998
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-03-13 10:08:39 +0530

    Sessions

--- app2.py
+++ app2.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__, template_folder='templates')
+app.secret_key = 'SOME KEY'
 
 @app.route('/')
 def index():
-  return render_template('two.html')
+  return render_template('two.html', message='Index')
+
+@app.route('/set_data')
+def set_data():
+  session['name'] = 'Akib'
+  session['other'] = 'Yo Bro'
+  return render_template('two.html', message='set_data')
+
+@app.route('/get_data')
+def get_data():
+  if 'name' in session.keys() and 'other' in session.keys():
+    name = session['name']
+    other = session['other']
+    return render_template('two.html', message=f'Name: {name}, Other: {other}')
+  else:
+    return render_template('two.html', message='No session found')
+  
+@app.route('/clear_session')
+def clear_session():
+  session.clear()
+  return render_template('two.html', message='Session cleared')
+  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port='3000', debug=True)
